a66221829838e0a8089c569a4f26d99509689f29
Andrii Olexandrovich <awispack@gmail.com>
2020-08-25 22:33:08 +0300

    Building objects schema (including layouts, flexipages, permission sets) + fixing CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ helpFunction() {
 }
 
 while getopts "u:" flag; do
-  case "${flag}" in
-  u) targetOrgUsername="${OPTARG}" ;;
+  case $flag in
+  u) targetOrgUsername=${OPTARG} ;;
   *) helpFunction ;;
   esac
 done
 
 # Print helpFunction in case parameters are empty;
-if [ -z "$targetOrgUsername" ]; then
+if [ -z $targetOrgUsername ]; then
   echo "Target org username is required"
   helpFunction
 fi
@@ -23,9 +23,15 @@ fi
 echo Target org username is: $targetOrgUsername
 
 #Deploy via Metadata API;
-export tmp_dir=mdapioutput
+tmp_dir=mdapioutput
+rm -rf $tmp_dir
 sfdx force:org:display -u $targetOrgUsername
 sfdx force:source:convert -d $tmp_dir
-sfdx force:mdapi:deploy -d $tmp_dir -u $targetOrgUsername -w 100
-rm -rf ${tmp_dir}
+commandResult=$(sfdx force:mdapi:deploy -d $tmp_dir -u $targetOrgUsername -w 100)
+echo ${commandResult}
+if echo ${commandResult} | grep -iqF failures; then
+  echo "Deployment failure!"
+  exit 1
+fi
+rm -rf $tmp_dir
 echo "Sources are deployed!"
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -7,14 +7,14 @@ helpFunction() {
 }
 
 while getopts "u:" flag; do
-  case "${flag}" in
-  u) targetOrgUsername="${OPTARG}" ;;
+  case $flag in
+  u) targetOrgUsername=${OPTARG} ;;
   *) helpFunction ;;
   esac
 done
 
 # Print helpFunction in case parameters are empty;
-if [ -z "$targetOrgUsername" ]; then
+if [ -z $targetOrgUsername ]; then
   echo "Target org username is required"
   helpFunction
 fi
@@ -23,12 +23,23 @@ fi
 echo Target org username is: $targetOrgUsername
 
 #Deploy via Metadata API;
-export tmp_dir=mdapioutput
+tmp_dir=mdapioutput
+rm -rf $tmp_dir
 sfdx force:org:display -u $targetOrgUsername
 sfdx force:source:convert -d $tmp_dir
-sfdx force:mdapi:deploy -d $tmp_dir -u $targetOrgUsername -w 100 -c
-rm -rf ${tmp_dir}
-
+validationCommandResult=$(sfdx force:mdapi:deploy -d $tmp_dir -u $targetOrgUsername -w 100 -c)
+echo ${validationCommandResult}
+if echo ${validationCommandResult} | grep -iqF failure; then
+  echo "Validation failure!"
+  exit 1
+fi
 # Run all tests
-sfdx force:apex:test:run -u "$targetOrgUsername" --wait 10
+runTestsCommandResult=$(sfdx force:apex:test:run -u $targetOrgUsername --wait 10)
+echo ${runTestsCommandResult}
+if echo ${runTestsCommandResult} | grep -iqF failure; then
+  echo "Tests failure!"
+  exit 1
+fi
+
+rm -rf $tmp_dir
 echo "Validated!"
